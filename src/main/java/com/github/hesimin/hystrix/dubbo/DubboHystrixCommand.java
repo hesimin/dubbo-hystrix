79871a609c3b1a49736f0416f000230f3f9b14f3
hesimin <hesimin@mftour.cn>
2017-06-30 10:54:53 +0800

    update parameter

--- src/main/java/com/github/hesimin/hystrix/dubbo/DubboHystrixCommand.java
+++ src/main/java/com/github/hesimin/hystrix/dubbo/DubboHystrixCommand.java
@@ -21,7 +21,7 @@ import org.slf4j.LoggerFactory;
  */
 public class DubboHystrixCommand extends HystrixCommand<Result> {
     private static       Logger logger                       = LoggerFactory.getLogger(DubboHystrixCommand.class);
-    private static final int    DEFAULT_THREADPOOL_CORE_SIZE = 100;
+    private static final int    DEFAULT_THREADPOOL_CORE_SIZE = 30;
     private Invoker<?> invoker;
     private Invocation invocation;
 
